2b8f9ac4932fa5eda49d8da91673d187667dc4f7
honki <276086064@qq.com>
2019-09-08 18:44:49 +0800

    goéo并发爬取页面

--- spider.go
+++ spider.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
+	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 //https://www.pengfu.com/xiaohua_1.html
 //https://www.pengfu.com/xiaohua_2.html
 
 
-func SpiderPage(i int)  {
+func SpiderPage(i int, ch chan int)  {
 	//url
 	url := "https://www.pengfu.com/xiaohua_" + strconv.Itoa(i) + ".html"
 	fmt.Printf("正在爬取第%d页的数据:%s\n", i, url)
@@ -26,12 +29,76 @@ func SpiderPage(i int)  {
 	if res == nil {
 		return
 	}
+	//首页的文章标题链接
 	joyUrl := res.FindAllStringSubmatch(result, -1)
+	fileTitle := make([]string, 0)
+	fileContent := make([]string, 0)
+
+
+
 	//fmt.Println(joyUrl)
 	for _, data := range joyUrl {
-		fmt.Println(data[1])
+		title, content, err1 := SpiderJoy(data[1])
+		if err1 != nil {
+			fmt.Println(err1)
+			continue
+		}
+
+		fileTitle = append(fileTitle, title)
+		fileContent = append(fileContent, content)
+	}
+	//fmt.Println(fileTitle)
+	JoyToFile(i, fileTitle, fileContent)
+	ch <- i
+}
+
+func JoyToFile(i int, title, content []string)  {
+	file, _ := os.Create(strconv.Itoa(i)+".txt")
+	defer file.Close()
+	//
+	n := len(title)
+	for i := 0; i < n; i++ {
+		file.WriteString(title[i]+"\n")
+		file.WriteString(content[i]+"\n")
+		file.WriteString("===========================\n")
 	}
 
+}
+
+
+func SpiderJoy(url string) (title , content string, err error) {
+	result, _ := HttpGet(url)
+	res := regexp.MustCompile(`<h1>(?s:(.*?))</h1>`)
+	if res == nil {
+		err = errors.New("compile error")
+		return
+	}
+	//get
+	tmpTitle := res.FindAllStringSubmatch(result, 1)
+	for _, data := range tmpTitle {
+		title = data[1]
+		title = strings.Replace(title, "\t", "", -1)
+		break
+	}
+
+	contentRes := regexp.MustCompile(`<div class="content-txt pt10">(?s:(.*?))<a id="prev" href="`)
+	if contentRes == nil {
+		err = errors.New("compile error")
+		return
+	}
+	//get
+	tmpContent := contentRes.FindAllStringSubmatch(result, 1)
+	for _, data := range tmpContent {
+		content = data[1]
+		content = strings.Replace(content, "\t", "", -1)
+		content = strings.Replace(content, "\n", "", -1)
+		content = strings.Replace(content, "&nbsp;", "", -1)
+		content = strings.Replace(content, " ", "", -1)
+		content = strings.Replace(content, "<br />", "", -1)
+		content = strings.Replace(content, "<br/>", "", -1)
+		break
+	}
+	return
 }
 
 func HttpGet(url string) (result string, err error) {
@@ -55,16 +122,21 @@ func HttpGet(url string) (result string, err error) {
 
 
 func DoWork(start, end int)  {
-	fmt.Println("spider ing")
+	fmt.Println("spidering")
 
+	ch := make(chan int)
 	for i := start; i <= end; i++ {
 		//定义函数爬主页面
-		SpiderPage(i)
+		go SpiderPage(i, ch)
+	}
+
+	for i := start; i <= end; i++ {
+		fmt.Printf("第%d个页面爬取完成\n", <-ch)
 	}
 
 }
 
 func main()  {
 
-	DoWork(1, 1)
+	DoWork(1, 5)
 }
